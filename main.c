d2dc4bc24acc3cff81fa5eb1aa44ab6cbdf5a033
cuiqiuyu <1474017454@qq.com>
2025-12-03 17:59:22 +0800

    成员A：更新main函数输出版本号

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-// 成员B新增的函数
-void print_collaborator() {
-    printf("Collaborator: Member B\n");
-}
 int main() {
     printf("Hello Git! This is version 1.0\n");
-    print_collaborator();  // 调用新增函数
+    print_collaborator();
     return 0;
 }
+===
