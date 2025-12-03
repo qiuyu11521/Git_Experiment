219aaf95ede3340f5839af3a74bdbcf0d348fd76
cuiqiuyu <1474017454@qq.com>
2025-12-03 17:43:46 +0800

    成员A：新增print_collaborator函数，显示协作成员信息

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+// 成员B新增的函数
+void print_collaborator() {
+    printf("Collaborator: Member B\n");
+}
 int main() {
-    printf("Hello Git! This is dev branch version\n");
+    printf("Hello Git! This is version 1.0\n");
+    print_collaborator();  // 调用新增函数
     return 0;
 }
